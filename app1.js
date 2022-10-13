6990e88b20576e4c8d5c924f7673d93019a6168e
nagarajan <nagarajannagarajan670@gmail.com>
2022-10-13 16:37:01 +0530

    add file

---
+++ app1.js
@@ -0,0 +1,9 @@
+export const name="nagaraj";
+
+export const getName=()=>{
+    return name;
+};
+
+export default ()=>{
+    return name;
+}
---
+++ app2.js
@@ -0,0 +1,4 @@
+import {name,getName} from"./app1.js";
+import getData from './app1.js';
+console.log(name,getName(),getData());
+
